95a99dd009cdf8153e6611375e74d717662726cf
Sofiane Bendoukha <bendoukha@dkrz.de>
2020-10-06 14:32:06 +0200

    Added test Singularity with conda.

---
+++ ml/Singularity.ml
@@ -0,0 +1,34 @@
+Bootstrap: docker
+From: continuumio/miniconda3:4.4.10
+
+%labels
+MAINTAINER statiksof
+
+%environment
+    # mount work and scratch
+    export SINGULARITY_BINDPATH="/scratch, /work"
+
+    # change conda path
+    export PATH="/opt/conda/bin:/usr/local/bin:/usr/bin:/bin:"
+    unset CONDA_DEFAULT_ENV
+    export ANACONDA_HOME=/opt/conda
+
+%post
+    # update and install pip
+    apt-get -y update
+
+    # clean apt
+    apt-get autoremove -y
+    apt-get clean
+
+    # install packages
+    export PATH=/opt/conda/bin:$PATH
+    conda config --add channels defaults
+    conda config --add channels conda-forge
+    conda install --yes jupyter
+    conda clean --index-cache --tarballs --packages --yes
+
+%runscript
+    echo "Starting the notebook ..."
+    echo "Open browser to localhost:8888"
+    exec /usr/local/bin/jupyter notebook --ip='*' --port=8888 --no-browser
